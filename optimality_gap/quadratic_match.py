6078dca09e343f55f0a3874bd34c63ba1b439d84
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-01-19 11:28:03 -0300

    add tqdm bars & move struct

---
+++ optimality_gap/quadratic_match.py
@@ -0,0 +1,93 @@
+import numpy as np
+import networkx as nx
+
+
+def aggregate_contributions(grant_contributions):
+    contrib_dict = {}
+    for contrib in grant_contributions:
+        contrib_tuple = tuple(*contrib.values())
+        user = contrib_tuple[1]
+        proj = contrib_tuple[2]
+        amount = contrib_tuple[3]
+        if proj not in contrib_dict:
+            contrib_dict[proj] = {}
+        contrib_dict[proj][user] = contrib_dict[proj].get(user, 0) + amount
+    return contrib_dict
+
+
+def get_totals_by_pair(contrib_dict):
+    tot_overlap = {}
+
+    # start pairwise match
+    for _, contribz in contrib_dict.items():
+        for k1, v1 in contribz.items():
+            if k1 not in tot_overlap:
+                tot_overlap[k1] = {}
+
+            # pairwise matches to current round
+            for k2, v2 in contribz.items():
+                if k2 not in tot_overlap[k1]:
+                    tot_overlap[k1][k2] = 0
+                tot_overlap[k1][k2] += (v1 * v2) ** 0.5
+
+    return tot_overlap
+
+
+def match_project(contribz, pair_totals, threshold):
+    proj_total = 0
+    for k1, v1 in contribz.items():
+        for k2, v2 in contribz.items():
+            if k2 > k1:
+                # quadratic formula
+                p = pair_totals[k1][k2]
+                if p == 0:
+                    continue
+                else:
+                    proj_total += (threshold + 1) * ((v1 * v2) ** 0.5) / p
+    return np.real(proj_total)
+
+
+
+def quadratic_match(G: nx.Graph,
+                    threshold: float) -> dict:
+    G = G.copy()
+    raw_contributions = G.edges(data=True)
+
+    contributions = []
+    for contrib in raw_contributions:
+        amount = contrib[2]['amount_per_period_usdt']
+        grant = contrib[1]
+        element = {grant: (None, contrib[0], contrib[1], amount)}
+        contributions.append(element)
+
+    contrib_dict = aggregate_contributions(contributions)
+    pair_totals = get_totals_by_pair(contrib_dict)
+    matches = {proj: match_project(contribz, pair_totals, threshold)
+               for proj, contribz in contrib_dict.items()}
+    return matches
+
+
+def quadratic_funding(G: nx.Graph,
+                      total_pot: float) -> dict:
+    G = G.copy()
+    grants = {label 
+              for label, node in G.nodes(data=True)
+              if node['type'] == 'grant'}
+    M = nx.get_node_attributes(G, 'match')
+
+    total_match = sum(M.values())
+    F = {}
+    if total_match > total_pot:
+        F = {g: total_pot * M[g] / total_match
+             for g in grants}
+    else:
+        if total_match == 0:
+            total_match = 1.0
+        F = {g: M[g] + (1 + np.log(total_pot / total_match) / 100)
+             for g in grants}
+    return F
+
+
+def total_quadratic_match(G: nx.Graph,
+                          threshold: float) -> float:
+    return sum(quadratic_match(G, threshold).values())
--- optimality_gap/subgraph_optimizer.py
+++ optimality_gap/subgraph_optimizer.py
@@ -1,27 +1,7 @@
 import networkx as nx
 import random
 from typing import Callable, List
-
-
-def test():
-    test_graph: nx.Graph = create_test_graph(1000, 42)
-    result: tuple = optimize_graph_connectivity(test_graph, utility_function)
-    print(f"Best subgraph: {result[0]}")
-    print(f"Best score: {result[1]}")
-
-
-def create_test_graph(n: int, seed: int) -> nx.Graph:
-    internet: nx.Graph = nx.random_internet_as_graph(n, seed=seed)
-    node_types: List[str] = ['source', 'destination']
-
-    for i in range(n):
-        internet.add_node(i, type=random.choice(node_types))
-
-    return internet
-
-
-def utility_function(G): return nx.wiener_index(G)
-
+from tqdm.auto import tqdm
 
 def optimize_graph_connectivity(subgraph: nx.Graph,
                                 utility_function: Callable[[nx.Graph], float]) -> tuple:
@@ -35,9 +15,10 @@ def optimize_graph_connectivity(subgraph: nx.Graph,
     best_subgraph: nx.Graph = subgraph.copy()
     best_score: float = utility_function(best_subgraph)
 
-    for edge in edges:
+    print("Optimizing subgraph")
+    for edge in tqdm(edges, desc='Sweeping edges'):
         src_node: str = edge[0]
-        for dst_node in destination_nodes:
+        for dst_node in tqdm(destination_nodes, desc='Sweeping destination nodes', leave=False):
             # Create a copy from the original graph for mutation
             temp_subgraph: nx.Graph = subgraph.copy()
             temp_subgraph.remove_edge(*edge)
@@ -52,7 +33,3 @@ def optimize_graph_connectivity(subgraph: nx.Graph,
                 best_subgraph: nx.Graph = temp_subgraph.copy()
 
     return (best_subgraph, best_score)
-
-
-if __name__ == "__main__":
-    test()
